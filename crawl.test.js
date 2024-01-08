319ecce5f57e7e331ce5bdfe6cf3db583351292a
Kane Pereira <kane.pereira18@gmail.com>
2024-01-08 00:52:33 -0500

    second test pass no end slash

--- crawl.test.js
+++ crawl.test.js
@@ -4,3 +4,7 @@ const { normalizeURL } = require('./crawl.js');
 test('turn https://blog.boot.dev/path/ to normalized blog.boot.dev/path', () => {
     expect(normalizeURL('https://blog.boot.dev/path/')).toBe('blog.boot.dev/path');
 })
+
+test('turn https://blog.boot.dev/path to normalized blog.boot.dev/path', () => {
+    expect(normalizeURL('https://blog.boot.dev/path/')).toBe('blog.boot.dev/path');
+})
