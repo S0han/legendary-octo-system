f2ba21909a05f724fa8398e61c81effce4c8931c
Kane Pereira <kane.pereira18@gmail.com>
2024-01-08 00:55:09 -0500

    get test to pass for http:// starting URL

--- crawl.js
+++ crawl.js
@@ -2,6 +2,9 @@ function normalizeURL(url) {
     if (url.startsWith('https://')) {
         url = url.replace('https://', '');
     }
+    else if (url.startsWith('http://')) {
+        url = url.replace('http://', '');
+    }
     
     if (url.endsWith('/')) {
         url = url.slice(0, -1);
