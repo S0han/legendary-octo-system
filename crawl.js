eebf559038fbf28e6ed6d68529790814bc72fda5
Kane Pereira <kane.pereira18@gmail.com>
2024-01-08 00:36:46 -0500

    passed first test to normalize URL

--- crawl.js
+++ crawl.js
@@ -1,5 +1,13 @@
 function normalizeURL(url) {
+    if (url.startsWith('https://')) {
+        url = url.replace('https://', '');
+    }
     
+    if (url.endsWith('/')) {
+        url = url.slice(0, -1);
+    }
+    
+    return url;
 }
 
 module.exports = {
--- crawl.test.js
+++ crawl.test.js
@@ -1,6 +1,6 @@
 const { test, expect } = require('@jest/globals');
 const { normalizeURL } = require('./crawl.js');
 
-test('turn https://blog.boot.dev/path/ to normalized blog.booth.dev/path', () => {
-    expect(url).toBe('blog.booth.dev/path')
+test('turn https://blog.boot.dev/path/ to normalized blog.boot.dev/path', () => {
+    expect(normalizeURL('https://blog.boot.dev/path/')).toBe('blog.boot.dev/path');
 })
