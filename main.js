09f9de713aec3625900df0a55a8e6c87390c5e18
Kane Pereira <kane.pereira18@gmail.com>
2024-01-20 02:06:50 -0500

    accept CLI args in main fcn using process.argv

---
+++ main.js
@@ -0,0 +1,18 @@
+const { argv } = require('node:process');
+
+function main() {
+    //we do 3 to check for 1 CLI arg because by default 
+    //there are always 2 args the node executable and the script.js file
+    if (process.argv.length < 3) {
+        console.error('Expected at least 1 CLI argument');
+        return
+    } else if (process.argv.length > 3) {
+        console.error('More than 1 CLI argument');
+        return
+    } else {
+        BASE_URL = process.argv[2]
+        console.log(`The crawler is starting at ${BASE_URL}`)
+    }
+}
+
+main()
