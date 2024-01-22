48a359e5865ebea5e71866300e16f7c22a0f5409
Kane Pereira <kane.pereira18@gmail.com>
2024-01-21 22:07:31 -0500

    fix up crawl page function to log all pages searched

--- main.js
+++ main.js
@@ -1,24 +1,20 @@
 const { argv } = require('node:process');
 const { crawlPage } = require('./crawl')
 
-function main() {
-    //test url underneath
-    // base_url = 'https://wagslane.dev'
-    
-    //we do 3 to check for 1 CLI arg because by default 
-    //there are always 2 args the node executable and the script.js file
+async function main() {
+   
     if (process.argv.length < 3) {
         console.error('Expected at least 1 CLI argument');
         return
     } else if (process.argv.length > 3) {
         console.error('More than 1 CLI argument');
         return
-    } else {
-        BASE_URL = process.argv[2]
-        console.log(`The crawler is starting at ${BASE_URL}`)
     }
     
-    const pages = crawlPage(process.argv[2]);
+    const BASE_URL = process.argv[2]
+    console.log(`The crawler is starting at ${BASE_URL}`)
+
+    const pages = crawlPage(BASE_URL, BASE_URL, {});
     console.log(pages);
 }
 
